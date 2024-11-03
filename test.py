1d482b65074536be928b59407f78c6395e2cf07c
Huy Dao Ngoc <huydao2802@gmail.com>
2024-11-03 15:21:31 +0700

    huydn: update without graphics in test.py

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import subprocess
 
 # List of x values
-x_values = [10, 100, 500, 1000]
+x_values = [10] # 50, 100
 
 for x in x_values:
     n = x + 50
-    command = f"python pacman.py -p ApproximateSARSAAgent -n {n} -x {x}"
+    # with out graphics
+    command = f"python pacman.py -p ApproximateSARSAAgent -n {n} -x {x} -q"
     print(f"Running command: {command}")
     subprocess.run(command, shell=True)
