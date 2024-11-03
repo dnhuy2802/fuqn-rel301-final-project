074d13a9789319034943632f354632300304195a
Huy Dao Ngoc <huydao2802@gmail.com>
2024-11-03 16:20:53 +0700

    huydn: remove source

--- pacman.py
+++ pacman.py
@@ -1,11 +1,3 @@
-# pacman.py
-# ---------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
-
 """
 Pacman.py holds the logic for the classic pacman game along with the main
 code to run a game.  This file is divided into three sections:
